f05a9a1f8ee374182cfd0fc34e106f30a9359f03
Veneza Dayao <dayao.maveneza@ue.edu.ph>
2020-11-23 22:38:49 +0800

    New Dashboard

--- dash-content.php
+++ dash-content.php
@@ -1,4 +1,12 @@
 <?php include'dash.php'?>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Dashboard - Brand</title>
+
+   
+    
+</head>
 
 <section class="page-content">
 
@@ -6,10 +14,13 @@
 <header class="title-dashboard">
 	 
           <h3 id="Anthony"><span style="color:black;">
-		 Dashboard</span>
-<h3>
+		 Dashboard</span></h3>
+              </header>
+    
+    <img class="dashboard" src="assets/img/logo.png"><strong>Barangay Montecarlo</strong>
+    <hr class="down">
 
-</header>
+
 
 </section>
 <!-------
@@ -24,7 +35,7 @@ Siniloan, Laguna</p><br><br>
 
 ------>
 
-  <section class="grid">
+<!--- <section class="grid">
     <article class="brgy-official-list">
 	<div class="header"> 
 	<h1>Baranagy Official List</h1>
@@ -45,11 +56,121 @@ Siniloan, Laguna</p><br><br>
 				</table>
 	
 	</article>
-    <article class="total-residents">
-	<div class="header1">
-	<h1>Total number of residents</h1>
-	</div>
-	</article>
+    ---->
+    <div class="card shadow border-left-warning py-2">
+    <div class="card-bodyregister">
+        <div class="row align-items-center no-gutters">
+            <div class="col mr-2">
+                <div class="text-uppercase text-warning font-weight-bold text-xs mb-1"><span>Total Registered Populations </span></div>
+                <div class="text-dark font-weight-bold h5 mb-0"><span>18</span></div>
+            </div>
+            <div class="col-auto"><i class="fas fa-users fa-2x text-gray-300"></i></div>
+        </div>
+    </div>
+      </div>
+    
+    <div class="card shadow border-left-warning py-3">
+    <div class="card-bodyfemale">
+        <div class="row align-items-center no-gutters">
+            <div class="col mr-2">
+                <div class="text-uppercase text-warning font-weight-bold text-xs mb-1"><span>Female </span></div>
+                <div class="text-dark font-weight-bold h5 mb-0"><span>9</span></div>
+            </div>
+            <div class="col-auto"><i class="fas fa-female fa-2x text-gray-300"></i></div>
+        </div>
+    </div>
+</div>
+    
+    <div class="card shadow border-left-warning py-4">
+    <div class="card-bodymale">
+        <div class="row align-items-center no-gutters">
+            <div class="col mr-2">
+                <div class="text-uppercase text-warning font-weight-bold text-xs mb-1"><span>Male</span></div>
+                <div class="text-dark font-weight-bold h5 mb-0"><span>9</span></div>
+            </div>
+            <div class="col-auto"><i class="fas fa-male fa-2x text-gray-300"></i></div>
+        </div>
+    </div>
+</div>
+    
+    <div class="card shadow border-left-warning py-5">
+    <div class="card-bodyvoters">
+        <div class="row align-items-center no-gutters">
+            <div class="col mr-2">
+                <div class="text-uppercase text-warning font-weight-bold text-xs mb-1"><span>Total Registered Voters </span></div>
+                <div class="text-dark font-weight-bold h5 mb-0"><span>10</span></div>
+            </div>
+            <div class="col-auto"><i class="fas fa-users fa-2x text-gray-300"></i></div>
+        </div>
+    </div>
+</div>
+    
+    <div class="card-header py-3">
+    <p class="text-primary m-0 font-weight-bold">Current Barangay Officials</p>
+</div><div class="card-body">
+    <div class="row">
+        <div class="col-md-6 text-nowrap">
+    </div>
+    <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
+        <table class="table dataTable my-0" id="dataTable">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>Office</th> 
+                </tr>
+            </thead>
+            <br>
+            <tbody>
+                <tr>
+                    <td>Airi Satou</td>
+                    <td>Accountant</td>
+                    <td>Tokyo</td>
+                </tr>
+                <tr>
+                    <td>Angelica Ramos</td>
+                    <td>Chief Executive Officer(CEO)</td>
+                    <td>Tokyo</td>
+                </tr>
+              
+            </tbody>
+        </table>
+    </div>
+    </div>
+</div>
+    <hr class="down2" />
+    <div class="card-header py-4">Puroks/Areas:</p>
+    </div>
+    <div class="card-body2">
+    <div class="row">
+        <div class="col-md-6 text-nowrap">
+
+    </div>
+    <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
+        <table class="table dataTable my-0" id="dataTable">
+            <thead>
+                <tr>
+                    <th>Area Name</th>
+                    <th>Area Count</th>
+ 
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Area 1</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Area 2</td>
+                    <td>1</td>
+                </tr>
+                <tr>                
+            </tfoot>
+        </table>
+    </div>
+    </div>
+</div>
+
     <article></article>
     <article></article>
     <article></article>
